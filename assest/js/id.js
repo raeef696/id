f756f9f700ba4a3b827b9e093f92027ab01d1da4
raeef696 <75561536+raeef696@users.noreply.github.com>
2022-04-30 07:26:59 -1100

    first commit

---
+++ assest/js/id.js
@@ -0,0 +1,8 @@
+// $('.navbar-light .navbar-nav .nav-link').on('click' ,function (){
+//     (this).addClass('active')
+// })
+
+
+// $('.navbar-light .navbar-nav .nav-link').on('click', function () {
+//     $(this).addClass('active').parent.siblings().removeClass('active');
+//   });
